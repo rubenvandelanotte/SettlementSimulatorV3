f791e0ce33010ac6e3f0ebebc46f495c7e5788a6
Ruben Vandelanotte <ruben.vdl1@gmail.com>
2025-05-09 12:44:55 +0200

    improved testing

--- TransactionAgent.py
+++ TransactionAgent.py
@@ -130,9 +130,9 @@ class TransactionAgent(Agent):
                     receipt_child_2.set_amount(min_amount_2)
                     print("----------------------------------------------------------------------------------------------")
                     print("delivery child 1 amount: " + str(delivery_child_1.get_amount()) + "delivery child 2 amount: " + str(delivery_child_2.get_amount()))
-                    print("receipt ")
-                    correctly_created_delivery = (delivery_child_1.get_amount() + delivery_child_2.get_amount == self.deliverer.get_amount())
-                    correctly_created_receipt = (receipt_child_1.get_amount() + receipt_child_2.get_amount == self.receiver.get_amount())
+                    print("receipt child 1 amount: " + str(receipt_child_1.get_amount()) + "delivery child 2 amount: " + str(receipt_child_1.get_amount()))
+                    correctly_created_delivery = (delivery_child_1.get_amount() + delivery_child_2.get_amount() == self.deliverer.get_amount())
+                    correctly_created_receipt = (receipt_child_1.get_amount() + receipt_child_2.get_amount() == self.receiver.get_amount())
                     if correctly_created_delivery and correctly_created_receipt:
                         print("-----------------------------------------------------------True-----------------------------------------------------------------------------------------")
                     else:
