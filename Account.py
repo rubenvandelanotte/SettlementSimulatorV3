be1869d1ed6c550a6ce784828857d8261c4c2d8d
MatthiasCiers <matthias.ciers@hotmail.com>
2025-05-19 15:05:35 +0200

    cleaned file

--- Account.py
+++ Account.py
@@ -13,17 +13,11 @@ class Account:
     def getAccountID(self):
         return self.accountID
 
-    def getCreditLimit(self):
-        return self.creditLimit
-
     def getBalance(self):
         return self.balance
 
     def getAccountType(self):
         return self.accountType
-
-    def getUsedCredit(self):
-        return self.usedCredit
 
     def get_newSecurities(self):
         return self.newSecurities
@@ -32,10 +26,11 @@ class Account:
         return self.original_balance
 
     def set_newSecurities(self, new:bool):
-        #gets set to true if new securities arrive, gets set to false if settlement fails.
+        # Gets set to true if new securities arrive, gets set to false if settlement fails.
         self.newSecurities = new
 
     def checkBalance(self, amount : int, securityType: str):
+        # Returns true if sufficient cash or securities, false if not
         if self.accountType == "Cash" and securityType == "Cash":
            return  self.balance + self.creditLimit >= amount
         elif self.accountType == securityType:
@@ -44,12 +39,15 @@ class Account:
             return False
 
     def getEffectiveAvailableCash(self):
+        # Returns the amount of available cash, with creditLimit
         if self.accountType == "Cash":
             return self.balance + self.creditLimit - self.usedCredit
         return 0
 
     def addBalance(self, amount:int, securityType:str):
-        #if cash account
+        # Add the amount to the relevant account
+
+        # If cash account:
         if self.accountType == securityType and self.accountType == "Cash":
             #check whether this account used credit already: condition only satisfies if usedCredit ==0
             if self.usedCredit == 0:
@@ -71,31 +69,26 @@ class Account:
                     self.balance = self.balance + remaining
                     self.set_newSecurities(True)
                     return amount
-
-        #if security account:
+        # If security account:
         elif self.accountType == securityType:
             self.balance = self.balance + amount
             self.set_newSecurities(True)
             return amount
-
         else:
             print("Error: account doesn't allow to add this type of assets")
             return 0
 
-
     def deductBalance(self, amount: int, securityType:str):
+        # Deduct the amount to the relevant account
 
-        #deduct cash
+        # If cash account:
         if self.accountType == securityType and self.accountType == "Cash":
             total_available = self.balance + (self.creditLimit - self.usedCredit)
             if total_available < amount:
                 return 0
-
-
             if self.balance >= amount:
                 self.balance= self.balance - amount
                 return amount
-
             else:
                 needed_from_credit = amount - self.balance
                 print(f"[PARTIAL CASH] Account {self.accountID} | Deducted {self.balance} from balance and {needed_from_credit} from credit.")
@@ -104,8 +97,7 @@ class Account:
                 print(f"[UPDATED] Account {self.accountID} | New Balance: {self.balance}, UsedCredit: {self.usedCredit}")
                 return amount
 
-
-        #deduct securities
+        # If security account:
         elif self.accountType == securityType:
             if self.balance >= amount:
                 self.balance = self.balance -amount
@@ -116,7 +108,6 @@ class Account:
                 #should never happen
                 print(f"[DEDUCT FAILED] Account {self.accountID} | Insufficient {securityType}. Balance: {self.balance}, Requested: {amount}")
                 return 0
-
         else:
             print(f"[ERROR] Account {self.accountID} | Cannot deduct {securityType} from account type {self.accountType}")
             return 0
